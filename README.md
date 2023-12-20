70da4fff822b8f4f71e1f3764f3c0c039215c213
Andrii Tagaiev <tagaew@gmail.com>
2023-12-20 13:25:15 +0100

    Added first version of the application

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# URLDownloader
+# URL Downloader
+
+## Overview
+
+This console application downloads files from URLs listed in a text file in parallel threads. The downloaded files are stored in a timestamped directory.
+
+## Usage
+
+### Prerequisites
+
+- [.NET 6.0 or later](https://dotnet.microsoft.com/download)
+
+### Steps to Use
+
+1. **Clone the Repository**
+
+   Clone this repository using the following command:
+
+   ```bash
+   git clone https://github.com/atagaew/URLDownloader.git
+
+2. **Navigate to the Project Directory**
+
+   ```bash
+   cd URLDownloader
+
+3. **Build the Application**
+
+   Execute the following command to build the application:
+
+   ```bash
+   dotnet build -c Release
+
+4. **Run the Application**
+
+   To use the program, execute the built executable file along with the file name containing the URLs:
+
+   ```bash
+   dotnet run -- -f <file_name_with_urls>
+   
+   Replace <file_name_with_urls> with the path to your file containing URLs.
+   Example:
+
+   ```bash
+   dotnet run -- -f links.txt
+
+   Ensure the file contains one URL per line.
+
+5. **Check Downloads**
+
+   After execution, the downloaded files will be saved in a newly created directory named with a timestamp in the format Download_yyyy-MM-dd-HH-mm-ss.
+
+
+
+
+
+
---
+++ URLDownloaderApp/URLDownloader/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace URLDownloader
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 0 || string.IsNullOrWhiteSpace(args[0]))
+            {
+                Console.WriteLine("Provide a file name containing URLs. Example: URLDownloader.exe links.txt");
+                return;
+            }
+
+            var fileName = args[0];
+
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine("File not found.");
+                return;
+            }
+
+            var urls = await File.ReadAllLinesAsync(fileName);
+            if (!urls.Any())
+            {
+                Console.WriteLine("File doesn't contain any URL.");
+                return;
+            }
+
+            await ProcessURLsAsync(urls);
+        }
+
+        private static async Task ProcessURLsAsync(string[] urls)
+        {
+            try
+            {
+                var directoryName = CreateDirectory();
+
+                int maxParallelDownloads = 10;
+
+                using SemaphoreSlim semaphore = new(maxParallelDownloads);
+                using HttpClient httpClient = new();
+                List<Task> downloadTasks = new();
+
+                foreach (string url in urls)
+                {
+                    await semaphore.WaitAsync();
+                    downloadTasks.Add(DownloadFileAsync(url, directoryName, httpClient, semaphore));
+                }
+
+                await Task.WhenAll(downloadTasks);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            Console.WriteLine("All files downloaded successfully.");
+        }
+
+        private static async Task DownloadFileAsync(string url, string directoryName, HttpClient httpClient, SemaphoreSlim semaphore)
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string fileNameFromUrl = Path.GetFileName(url);
+                    string filePath = Path.Combine(directoryName, fileNameFromUrl);
+
+                    using (Stream contentStream = await response.Content.ReadAsStreamAsync())
+                    using (Stream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        await contentStream.CopyToAsync(fileStream);
+                    }
+
+                    Console.WriteLine($"Downloaded {fileNameFromUrl}");
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to download {url}. Status code: {response.StatusCode}");
+                }
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        private static string CreateDirectory()
+        {
+            var timeStamp = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            var directoryName = $"Download_{timeStamp}";
+            Directory.CreateDirectory(directoryName);
+
+            return directoryName;
+        }
+    }
+
+}
